767fdc92c6ce2a60fcea9871b285e8eaf2210112
yivgen <evgene.020818@gmail.com>
2024-05-28 20:45:25 +0300

    Adjusted Deck and Subject name length

--- backend/flashcards/models.py
+++ backend/flashcards/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class Flashcard(models.Model):
-    question = models.CharField(max_length=300, blank=False, null=False)
+    question = models.CharField(max_length=500, blank=False, null=False)
     answer = models.TextField()
 
 class Deck(models.Model):
-    name = models.CharField(max_length=300, blank=False, null=False)
+    name = models.CharField(max_length=150, blank=False, null=False)
     flashcards = models.ManyToManyField(Flashcard, blank=True, related_name='deck_set')
 
 class Subject(models.Model):
-    name = models.CharField(max_length=300, blank=False, null=False)
+    name = models.CharField(max_length=150, blank=False, null=False)
     decks = models.ManyToManyField(Deck, blank=True, related_name='subject_set')
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,6 +3,7 @@ import Link from "@/node_modules/next/link";
 export default function Home() {
   return (
     <main>
+      <Link href="/subjects">Subjects</Link>
       <Link href="/decks">Decks</Link>
     </main>
   );

513f65884a65f89d700555d4073ec27a3306c301
yivgen <evgene.020818@gmail.com>
2024-06-01 18:50:17 +0300

    Switched to posgresql

--- backend/flashcards_app/settings.py
+++ backend/flashcards_app/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'flashcards_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'flashcards',
+        'USER': 'flashcards',
+        'PASSWORD': 'flashcards',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
